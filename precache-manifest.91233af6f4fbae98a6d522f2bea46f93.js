1852e43f2b577f7d2420816654bb577b56c56902
sudhir-jedhe <jedhesudhir@gmail.com>
2020-07-14 00:34:28 +0530

    Updates

---
+++ precache-manifest.91233af6f4fbae98a6d522f2bea46f93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780d33c4265d0debea02ddd041a1eb9d",
+    "url": "/myresume/index.html"
+  },
+  {
+    "revision": "6988c4828548793e5aca",
+    "url": "/myresume/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b484cc0ff1c10ea5f246",
+    "url": "/myresume/static/css/main.1e68dfc5.chunk.css"
+  },
+  {
+    "revision": "6988c4828548793e5aca",
+    "url": "/myresume/static/js/2.528886c6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/myresume/static/js/2.528886c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b484cc0ff1c10ea5f246",
+    "url": "/myresume/static/js/main.89c1ad12.chunk.js"
+  },
+  {
+    "revision": "7d273b942d25db80817c",
+    "url": "/myresume/static/js/runtime-main.24f13381.js"
+  },
+  {
+    "revision": "eb7f216127da1cf44c27f60acd3b50ee",
+    "url": "/myresume/static/media/sudhir.eb7f2161.jpg"
+  }
+]);
